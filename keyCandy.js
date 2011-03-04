c7a8dedfb38acf33e6e8edbcd1f76209c4941d95
Aaron McCall <aaron@agrinect.com>
2011-03-03 22:49:18 -0800

    keyCandy: gutted timeout class removal and standardized tooltip shower key to [ctrl] and hider key to be [esc].

--- keyCandy.js
+++ keyCandy.js
@@ -1,68 +1,55 @@
 /*
- * Key Candy - hotkeys for the visual crowd.
- * Provides tooltips for elements with accesskey attributes and focuses/activates 
- * elements with an accesskey without Alt, Alt + Shift, Command, etc.
- * (c) 2011 Aaron McCall. MIT license.                                                                  
+ * Key Candy makes your web app melt in your hands not on your mouse!
+ * Provides tooltips for elements with accesskey attributes and focuses/activates
+ * elements with an accesskey in a standardized, cross-browser, cross-platform way.
  */
+/* !(c) 2011 Aaron McCall. MIT license. */
 KeyCandy = (function($){
     var pub = {},
-    _target,        
+    _target,
     _class = 'keycandy',
-    _clear_timeout = clearTimeout,
     _remove_class = 'removeClass',
-    _remove_attr = 'removeAttr',
+    _has_class = 'hasClass',
     _default_parent = 'body',
-    _timeout_attr = 'data-keyCandy_timeout',
-    _clickable = function($el) {
-        return $el.is('a, :checkbox, :button, :submit, :radio, :reset')
-    },
     _tooltip_toggler = function(event){
         var $class_target = (_target) ? $(_target) : $(_default_parent),
             $target = $(event.target),
             _code = event.keyCode,
-            _timeout = $class_target.attr(_timeout_attr) || 0,
-            _matches = [17, 91, 93, 224];
-        if (_matches.indexOf(_code) > -1) {
-            $class_target.addClass(_class).attr(_timeout_attr, setTimeout(function(){
-                //console.log('firing timeout function to clear class: %s from parent with attribute: %s', _class, _timeout_attr);
-                $('.' + _class)[_remove_class](_class)[_remove_attr](_timeout_attr);
-            }, 5000));
+            _valid_accesskey = (_code > 46 && _code < 91);
+        if (_code === 17) {
+            $class_target.addClass(_class);
         } else {
-            $class_target[_remove_class](_class)[_remove_attr](_timeout_attr);                           
-            if ((_code > 46 && _code < 91) && !($target.is(':text, :password, textarea'))) {
-                var _selector = '[accesskey="' + String.fromCharCode(_code) + '"]',
-                    $el = $(_selector);
-                if ($el.length) {
-                    //console.log('Selector: %s is clickable: %s', _selector, _clickable($el));
-                    $el = ($el.is('label')) ? $('#' + $el.attr('for')) : $el;
-                    $el[(_clickable($el))?'click':'focus']();
-                    return false;
+            if ($class_target[_has_class](_class)) {
+                if (_code == 27 || _valid_accesskey) {
+                    if (_valid_accesskey) {
+                        var _selector = '[accesskey="' + String.fromCharCode(_code) + '"]',
+                            $el = $(_selector, '.' + _class);
+                        if ($el.length) {
+    //                        console.log('Selector: %s is %s', _selector, $el.attr('tagName'));
+                            $el = ($el.is('label')) ? $('#' + $el.attr('for')) : $el;
+                            $el[$el.is(':text, :password, textarea, select')?'focus':'click']();
+                            event.preventDefault();
+                        }
+                    }
+                    $class_target[_remove_class](_class);
                 }
             }
-        }
-        
-        if (_timeout) {
-            //console.log('clearing timeout %s', _timeout);
-            _clear_timeout(_timeout);
         }
     },
     init = function(_parent){
         _target = _parent || _default_parent;
-        $(window).keyup(_tooltip_toggler);
+        $(window).keydown(_tooltip_toggler);
         $(_target).click(function(){
           var $this = $(this);
-          if ($this.hasClass(_class)) {
-            _clear_timeout($this.attr(_timeout_attr));
-            $this[_remove_class](_class)[_remove_attr](_timeout_attr);
-          }
+          if ($this[_has_class](_class)) $this[_remove_class](_class);
         });
-        pub.initialized = true;
+//        pub.initialized = true;
     };
     pub = {
-        version: '0.4',
-        initialized: false,
+        version: '0.5',
+//        initialized: false,
         'init': init,
-        init_funcs: [init]
+//        init_funcs: [init]
     };
     return pub;
-})(window.jQuery || Zepto);
+})($ || jQuery);
