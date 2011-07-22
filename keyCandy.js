07a3a79bdb028884824702d8a9977edfc926824a
Aaron McCall <aaron@andyet.net>
2011-07-22 16:21:14 -0700

    Start of changes to allow activation when tooltips are not showing and from within typeable elements
    The typeables override is incomplete.

--- keyCandy.js
+++ keyCandy.js
@@ -4,59 +4,80 @@
  * elements with an accesskey in a standardized, cross-browser, cross-platform way.
  */
 /*! (c) 2011 Aaron McCall. 
+ *  Contributors: Beau Sorenson
  *  MIT license (see http://creativecommons.org/licenses/MIT/). 
  */
 KeyCandy = (function($){
     var pub = {},
+    _os = /Linux|Windows|Macintosh/.exec(navigator.userAgent).toString(),
     _target,
     _class = 'keycandy',
     _remove_class = 'removeClass',
     _has_class = 'hasClass',
     _default_parent = 'body',
+    _control_key = (_os==='Macintosh')?224:17,
+    _remove_key = 27,
+    _require_control_key = false,
+    _valid_override_keys = {'alt':1, 'ctrl':1, 'meta':1, 'shift':1},
+    _typeable_override_key = 'alt',
     _tooltip_toggler = function(event){
+        console.log("keyCode: %s, [alt]: %s [ctrl]: %s [meta]: %s [shift]: %s", event.keyCode, event.altKey, event.ctrlKey, event.metaKey, event.shiftKey);
         var $class_target = (_target) ? $(_target) : $(_default_parent),
             _target = event.target,
             _tag = _target.tagName.toLowerCase(),
             $target = $(_target),
             _code = event.keyCode,
-            _valid_accesskey = (_code > 46 && _code < 91),
-            _typeable = _tag === 'textarea' || (_tag === 'input' && /text|password/.test(_target.type) > -1);
-        if (_code === 17 && event.type == 'keydown' && !_typeable) {
+            _showtips_event = _code === _control_key && event.type == 'keydown',
+            _accesskey_event = ($class_target[_has_class](_class)||!_require_control_key) && event.type == 'keyup',
+            _valid_code = (_code > 46 && _code < 91),
+            _typeable = (_tag === 'textarea' || (_tag === 'input' && /text|password/.test(_target.type) > -1)) && !event[_typeable_override_key+'Key'];
+        if (_showtips_event && !_typeable) {
             $class_target.addClass(_class);
         } else {
-            if ($class_target[_has_class](_class) && event.type == 'keyup' && !_typeable) {
-                if (_code == 27 || _valid_accesskey) {
-                    if (_valid_accesskey) {
+            if (_accesskey_event && !_typeable) {
+                if (_code == _remove_key || _valid_code) {
+                    if (_valid_code) {
+                        console.log('testing for accesskey');
                         var _selector = '[accesskey="' + String.fromCharCode(_code) + '"]',
-                            $el = $(_selector, '.' + _class);
+                            $el = $(_selector);
+                        console.log("Selector: '%s'", _selector);
                         if ($el.length) {
-    //                        console.log('Selector: %s is %s', _selector, $el.attr('tagName'));
+                            console.log('activating accesskey');
                             $el = ($el.is('label')) ? $('#' + $el.attr('for')) : $el;
                             var _action = $el.is(':text, :password, textarea, select')?'focus':'click';
                             if ($el.is('a')) {
-                                $el.one('click', function(){ location.href = this.href });
+                                // Ensure the link has an href before changing location
+                                $el.one('click', function(){ this.href && (location.href = this.href) });
                             }
-//                            console.log('Triggering %s via .%s %s which contains %s elements', _action, _class, _selector, $el.length);
                             $el.trigger(_action);
-//                            if (_action == 'focus') event.preventDefault();
                         }
                     }
                     $class_target[_remove_class](_class);
+                    event.preventDefault();
+                    event.stopPropagation();
                 }
             }
         }
     },
-    init = function(_parent){
+    init = function(_parent, opt){
+        opt || (opt = {});
+        
+        // Control key and menu removal key overrides
+        opt.controlKey && (_control_key = opt.controlKey);
+        opt.removeKey  && (_remove_key = opt.removeKey);
+        opt.requireControlKey && (_require_control_key = opt.requireControlKey);
+        opt.typeableOverrideKey && _valid_override_keys[opt._typeable_override_key] && (_typeable_override_key = opt.typeableOverrideKey);
+        
         _target = _parent || _default_parent;
-        $(window).bind('keydown keyup', _tooltip_toggler);
+        //TODO: determine if we can eliminate the keyup, since that will allow us to activate the accesskey from a typeable element
+        $('window, input, textarea').bind('keydown keyup', _tooltip_toggler);
         $(_target).click(function(){
           var $this = $(this);
           if ($this[_has_class](_class)) $this[_remove_class](_class);
         });
-//        pub.initialized = true;
     };
     pub = {
-        version: '0.6',
+        version: '0.7',
         'init': init
     };
     return pub;
