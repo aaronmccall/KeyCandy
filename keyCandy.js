205136777260c653de2f7b419eb7212f26fe4d4d
Aaron McCall <aaron@andyet.net>
2011-07-24 00:53:39 -0700

    Leaner, meaner and smarter.

--- keyCandy.js
+++ keyCandy.js
@@ -17,68 +17,58 @@ KeyCandy = (function($){
     _has_class = 'hasClass',
     _default_parent = 'body',
     _control_key = (_os==='Macintosh')?224:17,
-    _require_control_key = false,
+    _req_control_key = false,
     _valid_override_keys = {'alt':1, 'ctrl':1, 'meta':1, 'shift':1},
-    _typeable_override_key = 'alt',
-    _tooltip_toggler = function(event){
-        console.log("keyCode: %s, [alt]: %s [ctrl]: %s [meta]: %s [shift]: %s", event.keyCode, event.altKey, event.ctrlKey, event.metaKey, event.shiftKey);
-        var $class_target = (_target) ? $(_target) : $(_default_parent),
-            _target = event.target,
-            _tag = _target.tagName.toLowerCase(),
-            $target = $(_target),
-            _code = event.keyCode,
-            _showtips_event = _code === _control_key,// && event.type == 'keydown',
-            _accesskey_event = ($class_target[_has_class](_class)||!_require_control_key),// && event.type == 'keyup',
-            _valid_code = (_code > 46 && _code < 91),
-            _typeable = (_tag === 'textarea' || (_tag === 'input' && /text|password/.test(_target.type) > -1)) && !event[_typeable_override_key+'Key'];
-        if (_showtips_event && !_typeable) {
-            $class_target.toggleClass(_class);
-        } else {
-            if (_accesskey_event && !_typeable) {
-                if (_code == _remove_key || _valid_code) {
-                    if (_valid_code) {
-                        console.log('testing for accesskey');
-                        var _selector = '[accesskey="' + String.fromCharCode(_code) + '"]',
-                            $el = $(_selector);
-                        console.log("Selector: '%s'", _selector);
-                        if ($el.length) {
-                            console.log('activating accesskey');
-                            $el = ($el.is('label')) ? $('#' + $el.attr('for')) : $el;
-                            var _action = $el.is(':text, :password, textarea, select')?'focus':'click';
-                            if ($el.is('a')) {
-                                // Ensure the link has an href before changing location
-                                $el.one('click', function(){ this.href && (location.href = this.href) });
+    _override_key = 'alt';
+    return {
+        version: '0.8',
+        init: function(_parent, opt){
+            opt || (opt = {});
+
+            // Control key and menu removal key overrides
+            opt.ctrlKey && (_control_key = opt.ctrlKey);
+            opt.reqCtrlKey && (_req_control_key = opt.reqCtrlKey);
+            opt.overrideKey && _valid_override_keys[opt.overrideKey] && (_override_key = opt.overrideKey);
+
+            _target = _parent || _default_parent;
+            $(window).bind('keydown', function(event){
+                var $class_target = (_target) ? $(_target) : $(_default_parent),
+                    _target = event.target,
+                    _tag = _target.tagName.toLowerCase(),
+                    $target = $(_target),
+                    _code = event.keyCode,
+                    _do_showtips = _code === _control_key,// && event.type == 'keydown',
+                    _accesskey_event = ($class_target[_has_class](_class)||!_req_control_key),// && event.type == 'keyup',
+                    _valid_code = (_code > 46 && _code < 91),
+                    _typeable = (_tag === 'textarea' || (_tag === 'input' && /text|password|email|search|tel|url/.test(_target.type) > -1)) && !event[_override_key+'Key'];
+                if (_do_showtips && !_typeable) {
+                    $class_target.toggleClass(_class);
+                } else {
+                    if (_accesskey_event && !_typeable) {
+                        if (_valid_code) {
+                            if (_valid_code) {
+                                var _selector = '[accesskey="' + String.fromCharCode(_code) + '"]',
+                                    $el = $(_selector);
+                                if ($el.length) {
+                                    $el = ($el.is('label')) ? $('#' + $el.attr('for')) : $el;
+                                    var _action = $el.is(':text, :password, textarea, select')?'focus':'click';
+                                    if ($el.is('a')) {
+                                        // Ensure the link has an href before changing location
+                                        $el.one('click', function(){ this.href && (location.href = this.href) });
+                                    }
+                                    $el.trigger(_action);
+                                    return false;
+                                }
                             }
-                            $el.trigger(_action);
-                            event.preventDefault();
-                            event.stopPropagation();
                         }
+                        $class_target[_remove_class](_class);
                     }
                 }
-                $class_target[_remove_class](_class);
-            }
-        }
-    },
-    init = function(_parent, opt){
-        opt || (opt = {});
-        
-        // Control key and menu removal key overrides
-        opt.controlKey && (_control_key = opt.controlKey);
-        opt.requireControlKey && (_require_control_key = opt.requireControlKey);
-        opt.typeableOverrideKey && _valid_override_keys[opt._typeable_override_key] && (_typeable_override_key = opt.typeableOverrideKey);
-        
-        _target = _parent || _default_parent;
-        //TODO: determine if we can eliminate the keyup, since that will allow us to activate the accesskey from a typeable element
-        $(window).bind('keydown', _tooltip_toggler);
-        $(_target).click(function(){
-          var $this = $(this);
-          if ($this[_has_class](_class)) $this[_remove_class](_class);
-        });
-    };
-    pub = {
-        version: '0.7',
-        init: init,
+            });
+            $(_target).click(function(){
+              $(this)[_remove_class](_class);
+            });
+        },
         os: _os
     };
-    return pub;
 })($ || jQuery);
