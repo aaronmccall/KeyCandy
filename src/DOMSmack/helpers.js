0660f50cef4c0739e896f7c67a60fbab8cdedda5
Aaron McCall <aaron@andyet.net>
2012-03-14 12:00:25 -0700

    Added __any() an Array.some shim

--- src/DOMSmack/helpers.js
+++ src/DOMSmack/helpers.js
@@ -5,6 +5,28 @@
     var ArrProto = Array.prototype,
         breaker = {};
 
+    // ## __any ##
+    // Array.some-like functionality provided either by the native method
+    // or by an `__each`-based fallback
+    // ### Args:
+    // `array {Array}`: the array we are operating on
+    // `callback {Function}`: the tester function to call on each member of `array`
+    // `context {Object}`: the [optional] `this` object for `callback`
+    function __any(array, callback, context) {
+        var newFunc = (ArrProto.some) ?
+            function (array, callback) { 
+                return ArrProto.some.call(array, callback, context)
+            } :
+            function (array, callback, context) { 
+                var result = false;
+                __each(array||[], function (val, idx, list) {
+                    if (result || (result = callback.call(context, val, idx, list))) return breaker;
+                }, context);
+                return !!result;
+            }
+        __any = newFunc;
+        return newFunc(array, callback, context);
+    }
 
     function __slice(obj) { 
         return Array.prototype.slice.call(obj); 
