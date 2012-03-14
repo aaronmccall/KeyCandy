4a36b10e52cedc20083583f57094d4fac52cffc0
Aaron McCall <aaron@andyet.net>
2012-03-14 11:55:31 -0700

    Add docs; make multi-element capable, etc.

--- src/DOMSmack/attributes.js
+++ src/DOMSmack/attributes.js
@@ -1,77 +1,49 @@
-    function hasClass(el, cls) { 
-        return classRE(cls).test(el.className); 
-    }
+// DOMSmack attribute accessors and manipulators
+    // source: src/DOMSmack/attributes.js
 
-    proto.hasClass = function(cls) { 
-        if (this.length ===1) return hasClass(this.el, cls); 
-        var has_class = false;
-        __each(this.els, function (el) {
-            if (hasClass(el, cls)) has_class = true;
-        }, this);
-        return has_class;
-    };
+    // Map HTML attribute names to DOM property names
+    var propertyFix = {
+        tabindex:        'tabIndex',
+        readonly:        'readOnly',
+        'for':           'htmlFor',
+        'class':         'className',
+        maxlength:       'maxLength',
+        cellspacing:     'cellSpacing',
+        cellpadding:     'cellPadding',
+        rowspan:         'rowSpan',
+        colspan:         'colSpan',
+        usemap:          'useMap',
+        frameborder:     'frameBorder',
+        contenteditable: 'contentEditable'
+    },
 
-    function addClass(el, cls) {
-        var classList;
-        if (!hasClass(el, cls)) {
-            classList = el.className.split(/\s+/g);
-            classList.push(cls);
-            el.className = classList.join(' ');
-        }
-    }
-
-    proto.addClass = function(cls){
-        __each(this.els, function (el) { addClass(el, cls); }, this);
-        return this;
-    };
-
-    function removeClass(el, cls) {
-        el.className = el.className.replace(classRE(cls), '').replace(/^\s+|\s+$/g, '');
-    }
-
-    proto.removeClass = function(cls) {
-        __each(this.els, function (el) { removeClass(el, cls); }, this);
-        return this;
-    };
-
-    function toggleClass(el, cls) {
-        if (hasClass(el, cls)) {
-            removeClass(el, cls);
-        } else {
-            addClass(el, cls);
-        }
-    }
-
-    proto.toggleClass = function(cls) {
-        for (var i=0; i<this.length; i++) toggleClass(this[i], cls);
-        return this;
-    };
+    // Old IE versions require an additional parameter to getAttribute
+    getAttributeParamFix = { width: 1, height: 1, src: 1, href: 1 };
 
 
     function hasGetAttribute() {
-        var div = document.createElement('div');
-        div.innerHTML = '<a href="/example"></a>';
+        var div = document.createElement('div'), newFunc;
+        div.innerHTML = '<a href="#"></a>';
 
-        if (div.childNodes[0].getAttribute('href') === '/example') {
-            hasGetAttribute = function() { return true };
+        if (div.childNodes[0].getAttribute('href') === '#') {
+            newFunc = function() { return true; };
             return true;
         }
 
         // Helps IE release memory associated with the div
         div = null;
-        hasGetAttribute = function() { return false };
+        newFunc = function() { return false; };
+        hasGetAttribute = newFunc;
         return false;
     }
 
     function setAttr(el, attr, val) {
-        var newFunc = hasGetAttribute()
-            ? function (el, attr, val) {
+        var newFunc = hasGetAttribute() ? 
+            function (el, attr, val) {
                 return el.setAttribute(attr, val);
-            }
-            : function (el, attr, val) {
-                if (propertyFix[name]) {
-                    name = propertyFix[name];
-                }
+            } : 
+            function (el, attr, val) {
+                if (propertyFix[name]) name = propertyFix[name];
 
                 if (name === 'value' && element.nodeName === 'BUTTON') {
                     return element.getAttributeNode(name).nodeValue = value;
@@ -87,19 +59,13 @@
         // Build a customized attribute getter for the current browser.
         var newFunc = hasGetAttribute() ?
             // Browser supports standard attribute access
-            function (el, attr) {
-                return el.getAttribute(attr);
-            } : 
+            function (el, attr) { return el.getAttribute(attr); } : 
             function (el, attr) {
                 // Swap HTML attribute name for DOM property name, if required.
-                if (propertyFix[attr]) {
-                    attr = propertyFix[attr];
-                }
+                if (propertyFix[attr]) attr = propertyFix[attr];
 
                 // Old IE versions require the extra param for some attributes.
-                if (getAttributeParamFix[attr]) {
-                    return el.getAttribute(attr, 2);
-                }
+                if (getAttributeParamFix[attr]) return el.getAttribute(attr, 2);
 
                 // Old IE versions do weird things with button.value.
                 if (attr === 'value' && el.nodeName === 'BUTTON') {
@@ -117,36 +83,91 @@
     }
 
     function removeAttr(el, attr) {
-        var newFunc = hasGetAttribute()
-            ? function (el, attr) {
+        var newFunc = hasGetAttribute() ? 
+            function (el, attr) {
                 if ('1-9'.indexOf(''+el.nodeType) < 0) return;
                 setAttr(el, attr, '');
                 el.removeAttributeNode(el.getAttributeNode(attr));
-            }
-            : function (el, attr) {
+            } : 
+            function (el, attr) {
                 el.removeAttribute(attr);
             };
         removeAttr = newFunc;
         return newFunc(el, attr);
     }
 
-    proto.attr = function(name, value){
+    proto.attr = function (name, value) {
         if (value === undefined) return getAttr(this.el, name) ;
 
-        for (var i=0; i<this.length; i++) {
+        __each(this.els, function (el) {
             if (value === null) {
-                removeAttr(this[i], name);
+                removeAttr(el, name);
             } else {
-                setAttr(this[i], name, value);
+                setAttr(el, name, value);
             }
-        }
+        });
+
         return this;
     };
 
-    proto.prop = function(name, value){
-        var prop = (propertyFix[name]) ? propertyFix[name] : name;
-        if (prop in this.el) {
-            if (typeof value === undefined ) return this.el[prop];
-            this.el[prop] = value;
+
+    function hasClass(el, cls) { 
+        return classRE(cls).test(el.className); 
+    }
+
+    proto.hasClass = function (cls) { 
+        if (this.length ===1) return hasClass(this.el, cls); 
+        var has_class = false;
+        __each(this.els, function (el) {
+            if (hasClass(el, cls)) has_class = true;
+        }, this);
+        return has_class;
+    };
+
+    function addClass(el, cls) {
+        var classList;
+        if (!hasClass(el, cls)) {
+            classList = (el.className||"").split(/\s+/g);
+            classList.push(cls);
+            el.className = classList.join(' ');
         }
+    }
+
+    proto.addClass = function (cls) {
+        __each(this.els, function (el) { addClass(el, cls) }, this);
+        return this;
+    };
+
+    function removeClass(el, cls) {
+        el.className = el.className.replace(classRE(cls), '').replace(/^\s+|\s+$/g, '');
+    }
+
+    proto.removeClass = function (cls) {
+        __each(this.els, function (el) { removeClass(el, cls) }, this);
+        return this;
+    };
+
+    function toggleClass(el, cls) {
+        return (hasClass(el, cls)) ? removeClass(el, cls) : addClass(el, cls)
+    }
+
+    proto.toggleClass = function (cls) {
+        __each(this.els, function (el) { 
+            toggleClass(el, cls); 
+        });
+        return this;
+    };
+
+    function prop(el, name, value) {
+        var prop = (propertyFix[name]) ? propertyFix[name] : name;
+        if (prop in el) {
+            if (typeof value === undefined ) return el[prop];
+            el[prop] = value;
+            return this;
+        }
+    };
+
+
+    proto.prop = function (name, value) {
+        __each(this.els, function (el) { prop(el, name, value); }, this);
     };
