6f889ec0c1c91f3f309a7069734a4c9d4edd0ee2
Aaron McCall <aaron@andyet.net>
2012-03-14 11:57:09 -0700

    Refactor to use helpers where appropriate

--- src/DOMSmack/events.js
+++ src/DOMSmack/events.js
@@ -1,11 +1,19 @@
+// ## DOMSmack Events ##
+    //
+    // *source: src/DOMSmack/events.js*
 
-
+    // ## bind ##
+    // Function to setup event handlers on elements
+    // ### Args:
+    //  * `el {DOMElement}`: the element to add event listener to
+    //  * `type {String}`: the event type to add a listener for
+    //  * `callback {Function}`: called when event fires
     function bind(el, type, callback) {
-        var newFunc = el.addEventListener
-            ? function (el, type, callback) {
+        var newFunc = (el.addEventListener) ?
+            function (el, type, callback) {
                 el.addEventListener(type, callback, true);
-            }
-            : function (el, type, callback) {
+            } :
+            function (el, type, callback) {
                 el.attachEvent('on'+type, callback);
             };
 
@@ -15,25 +23,25 @@
 
     proto.bind = function(type, callback){
         var elKey;
-        for (var i=0; i<this.length; i++) {
-            if (__indexOf(__el_list, this[i]) < 0) __el_list.push(this[i]);
-            elKey = __indexOf(__el_list, this[i]);
+        __each(this.els, function (el, idx) {
+            if (__indexOf(__el_list, el) < 0) __el_list.push(el);
+            elKey = __indexOf(__el_list, el);
             __el_evt_map[elKey] = __el_evt_map[elKey] || {};
             __el_evt_map[elKey][type] = __el_evt_map[elKey][type] || [];
             if (__indexOf(__el_evt_map[elKey][type], callback) < 0) {
-                bind(this[i], type, callback);
+                bind(el, type, callback);
                 __el_evt_map[elKey][type].push(callback);
             }
-        }
+        }, this);
         return this;
     };
 
     function unbind(el, type, handle) {
-        var newFunc = document.removeEventListener
-            ? function( el, type, handle ) {
+        var newFunc = (document.removeEventListener) ?
+        function( el, type, handle ) {
             if ( el.removeEventListener ) el.removeEventListener( type, handle, false );
-        }
-            : function( el, type, handle ) {
+        } : 
+        function( el, type, handle ) {
             if ( el.detachEvent ) el.detachEvent( "on" + type, handle );
         };
         unbind = newFunc;
@@ -56,8 +64,8 @@
     };
 
     proto.one = function(type, func) {
-        var handle = __bind(this.el, func),
-            wrapper = __bind(this, function(event){
+        var handle = bind(this.el, func),
+            wrapper = bind(this, function(event){
                 handle(event);
                 this.unbind(type, wrapper);
             });
